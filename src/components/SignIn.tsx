d89b0f16d54c3dd4c1d9d9cfe0ec41b4549fa8fe
laviveti <ti.desenvolvimento@lavive.com.br>
2023-07-03 19:58:14 -0300

    feat: Iniciando UserAuthForm

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Icons } from "./Icons";
 
 export const SignIn = ({}) => {
@@ -9,6 +10,19 @@ export const SignIn = ({}) => {
         <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a Breadit account and agree to our
           User Agreement and Privacy Policy.
+        </p>
+
+        {/* sign in form */}
+        {/* <UserAuthForm /> */}
+
+        <p className="px-8 text-center text-sm text-zinc-700 ">
+          New to Breadit?{" "}
+          <Link
+            href="/sign-up"
+            className="hover:text-zinc-800 text-sm underline underline-offset-4 "
+          >
+            Sign Up
+          </Link>
         </p>
       </div>
     </div>
