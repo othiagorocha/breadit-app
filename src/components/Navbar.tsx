9a5fca11498031e1d3a760b940b48dd1ec3390c1
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-07-05 15:22:23 -0300

    feat: Sessão do usuário na Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,38 @@
-import Link from "next/link";
-import React from "react";
-import { Icons } from "./Icons";
-import { buttonVariants } from "./ui/Button";
+import Link from 'next/link';
+import React from 'react';
+import { Icons } from './Icons';
+import { buttonVariants } from './ui/Button';
+import { getAuthSession } from '@/lib/auth';
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const session = await getAuthSession();
+
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2 ">
-      <div className="container max-w-7x h-full mx-auto flex items-center justify-between gap-2 ">
+    <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2 '>
+      <div className='container max-w-7x h-full mx-auto flex items-center justify-between gap-2 '>
         {/* logo */}
-        <Link href="/" className="flex gap-2 items-center ">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6 " />
-          <p className="hidden text-zinc-700 text-sm font-medium md:block ">
+        <Link
+          href='/'
+          className='flex gap-2 items-center '
+        >
+          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6 ' />
+          <p className='hidden text-zinc-700 text-sm font-medium md:block '>
             Breadit
           </p>
         </Link>
 
         {/* search bar */}
 
-        <Link href={"/sign-in"} className={buttonVariants()}>
-          Sign In
-        </Link>
+        {session ? (
+          <p>youre logged in</p>
+        ) : (
+          <Link
+            href={'/sign-in'}
+            className={buttonVariants()}
+          >
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
