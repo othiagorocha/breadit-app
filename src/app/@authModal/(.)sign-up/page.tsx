80e24b126cb2db8b87fcaaddb9d40137bc95da02
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-07-06 15:14:49 -0300

    feat: Moldando `schema.prisma`

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -1,5 +1,5 @@
 import { CloseModal } from '@/components/CloseModal';
-import { SignIn } from '@/components/SignIn';
+import { SignUp } from '@/components/SignUp';
 import { FC } from 'react';
 
 const page: FC = () => {
@@ -10,7 +10,7 @@ const page: FC = () => {
           <div className='absolute top-4 right-4'>
             <CloseModal />
           </div>
-          <SignIn />
+          <SignUp />
         </div>
       </div>
     </div>
